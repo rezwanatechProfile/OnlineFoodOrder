2889f716aa0f0b2058478953adad4b5150bdbdb0
rezwanatechProfile <rezwanatech@gmail.com>
2023-06-06 23:23:33 -0400

    add food item in the cart and delete functionality complete

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # ADD TO CART
     path('add_to_cart/<int:food_id>/', views.add_to_cart, name='add_to_cart'),
     # DECREASE CART
-    # path('decrease_cart/<int:food_id>/', views.decrease_cart, name='decrease_cart'),
+    path('decrease_cart/<int:food_id>/', views.decrease_cart, name='decrease_cart'),
     # DELETE CART ITEM
     # path('delete_cart/<int:cart_id>/', views.delete_cart, name='delete_cart'),
     
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
+from marketplace.context_processors import get_cart_counter
 from menu.models import Category, FoodItem
 from vendor.models import Vendor
 from django.db.models import Prefetch
+from .models import Cart
 
 # Create your views here.
 
@@ -35,14 +37,16 @@ def vendor_detail(request, vendor_slug):
     # today = today_date.isoweekday()
     
     # current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
-    # if request.user.is_authenticated:
-    #     cart_items = Cart.objects.filter(user=request.user)
-    # else:
-    #     cart_items = None
+
+    # to get the acces in cart items
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
     context = {
         'vendor': vendor,
         'categories': categories,
-        # 'cart_items': cart_items,
+        'cart_items': cart_items,
         # 'opening_hours': opening_hours,
         # 'current_opening_hours': current_opening_hours,
     }
@@ -50,28 +54,56 @@ def vendor_detail(request, vendor_slug):
 
 
 def add_to_cart(request, food_id):
-    return HttpResponse('testing')
-    # if request.user.is_authenticated:
-    #     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-    #         # Check if the food item exists
-    #         try:
-    #             fooditem = FoodItem.objects.get(id=food_id)
-    #             # Check if the user has already added that food to the cart
-    #             try:
-    #                 chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
-    #                 # Increase the cart quantity
-    #                 chkCart.quantity += 1
-    #                 chkCart.save()
-    #                 return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
-    #             except:
-    #                 chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
-    #                 return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
-    #         except:
-    #             return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
-    #     else:
-    #         return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    # Increase the cart quantity
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
+                except:
+                    chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
         
-    # else:
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
+        
+
+def decrease_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    if chkCart.quantity > 1:
+                        # Decrease the cart quantity
+                        chkCart.quantity -= 1
+                        chkCart.save()
+                    else:
+                        chkCart.delete()
+                        chkCart.quantity = 0
+                    return JsonResponse({'status': 'Success', 'message': 'Decreased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
+                except:
+                    return JsonResponse({'status': 'Failed', 'message': 'You do not have item in your cart!'})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+        
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
         
 
     
